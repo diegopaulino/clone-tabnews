27493663fa1ab6aec99cbea417904aba0f1f3bbc
Diego <diegopaulino_3@hotmail.com>
2025-07-29 21:28:10 -0300

    add  endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,9 @@
 import { Client } from "pg";
 
-const query = async (queryObject) => {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
+async function query(queryObject) {
+  let client;
   try {
-    await client.connect();
-
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -21,8 +12,33 @@ const query = async (queryObject) => {
   } finally {
     await client.end();
   }
-};
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValue(),
+  });
+
+  await client.connect();
+  return client;
+}
 
 export default {
   query: query,
+  getNewClient,
 };
+
+function getSSLValue() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
